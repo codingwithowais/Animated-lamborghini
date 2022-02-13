ff6c93dd49d298820bc39c0bdd1ad0e57d890fd8
codingwithowais <owaisahmad1399@Animated-lamborghini.com>
2022-02-13 19:34:55 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  
+background=document.addEventListener('click',()=>{
+   
+    
+    audio=document.createElement('audio')
+    audio.setAttribute('src','sound.mp3');
+    audio.loop=true
+    audio.play();
+
+});
+
+
+
